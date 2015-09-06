fec374d2011bce39975ce7662e63e0bbd4ff9bbc
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 22:05:33 +0200

    Use AVA for testing

--- test.js
+++ test.js
@@ -1,33 +1,39 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 
 Math.fround = undefined;
 var fround = require('./');
 
-it('if x is NaN, return NaN', function () {
-	assert(isNaN(fround(NaN)));
+test('if x is NaN, return NaN', function (t) {
+	t.true(isNaN(fround(NaN)));
+	t.end();
 });
 
 [Infinity, -Infinity].forEach(function (x) {
-	it('If x is ' + x + ', return x', function () {
-		assert.strictEqual(fround(x), x);
+	test('If x is ' + x + ', return x', function (t) {
+		t.is(fround(x), x);
+		t.end();
 	});
 });
 
-it('If x is +0, return x', function () {
-	assert.strictEqual(fround(+0), +0);
+test('If x is +0, return x', function (t) {
+	t.is(fround(+0), +0);
+	t.end();
 });
 
-it('If x is -0, return x', function () {
-	assert.strictEqual(fround(-0), -0);
+test('If x is -0, return x', function (t) {
+	t.is(fround(-0), -0);
+	t.end();
 });
 
-it('Returns the nearest single precision float representation of x', function () {
+test('Returns the nearest single precision float representation of x', function (t) {
 	[
 		[0, 0],
 		[1, 1],
 		[1.337, 1.3370000123977661]
 	].forEach(function (x) {
-		assert.strictEqual(fround(x[0]), x[1]);
+		t.is(fround(x[0]), x[1]);
 	});
+
+	t.end();
 });
