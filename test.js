6845ac83ebb9d35bc87b8eb6086b5b5449e03c66
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 11:34:23 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,37 +1,36 @@
-'use strict';
-var test = require('ava');
+import test from 'ava';
 
 Math.fround = undefined;
-var fround = require('./');
+import fround from './';
 
-test('if x is NaN, return NaN', function (t) {
+test('if x is NaN, return NaN', t => {
 	t.true(isNaN(fround(NaN)));
 	t.end();
 });
 
-[Infinity, -Infinity].forEach(function (x) {
-	test('If x is ' + x + ', return x', function (t) {
+[Infinity, -Infinity].forEach(x => {
+	test('If x is ' + x + ', return x', t => {
 		t.is(fround(x), x);
 		t.end();
 	});
 });
 
-test('If x is +0, return x', function (t) {
+test('If x is +0, return x', t => {
 	t.is(fround(+0), +0);
 	t.end();
 });
 
-test('If x is -0, return x', function (t) {
+test('If x is -0, return x', t => {
 	t.is(fround(-0), -0);
 	t.end();
 });
 
-test('Returns the nearest single precision float representation of x', function (t) {
+test('Returns the nearest single precision float representation of x', t => {
 	[
 		[0, 0],
 		[1, 1],
 		[1.337, 1.3370000123977661]
-	].forEach(function (x) {
+	].forEach(x => {
 		t.is(fround(x[0]), x[1]);
 	});
 
